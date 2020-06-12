189c47aa8a1d579c6d136a3a8cd6b634ac1d4806
Sebastian Schroeder <sebastian@chatthread.net>
2020-06-13 00:47:42 +0200

    add file server handler for serving static files

--- app.go
+++ app.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	log.Print("Running chatTHREAD app...")
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/page/", pagePresentationHandler)
 	http.HandleFunc("/api/page/", pageApiHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
