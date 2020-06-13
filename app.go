baf027ce65ffbb8a96b5f21b60dc86a0daad5ac2
Sebastian Schroeder <sebastian@chatthread.net>
2020-06-14 00:41:38 +0200

    serve static file gzip compressed

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	log.Print("Running chatTHREAD app...")
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", gzipCompression(fs)))
 	http.HandleFunc("/page/", pagePresentationHandler)
 	http.HandleFunc("/api/page/", pageApiHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
---
+++ gzip.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func gzipCompression(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			h.ServeHTTP(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		h.ServeHTTP(gzipResponseWriter{Writer: gz, ResponseWriter: w}, r)
+	})
+}
